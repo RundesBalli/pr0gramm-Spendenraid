ed9cf9629c6fa68ad91b22876e30700cabbfb4f1
RundesBalli <rundesballi@rundesballi.com>
2020-03-08 10:01:00 +0100

    Kopierfehler korrigiert

--- public/src/toggleMenu.js
+++ public/src/toggleMenu.js
@@ -1,5 +1,5 @@
 function toggleMenu() {
-  var x = document.getElementById("sidebar");
+  var x = document.getElementById("nav");
   if (x.className === "") {
       x.className += "responsive";
   } else {
