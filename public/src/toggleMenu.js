486bcbd69300929911a851bfb0ece6dd84bdb3ac
RundesBalli <rundesballi@rundesballi.com>
2020-03-27 15:14:53 +0100

    Mobilnavigation gefixt. Danke an @NLDev

--- public/src/toggleMenu.js
+++ public/src/toggleMenu.js
@@ -1,10 +1,6 @@
 function toggleMenu() {
   var x = document.getElementById("nav");
-  if (x.className === "") {
-      x.className += "responsive";
-  } else {
-      x.className = "";
-  }
+  x.classList.toggle("responsive");
 }
 
 document.addEventListener("DOMContentLoaded", function () {
