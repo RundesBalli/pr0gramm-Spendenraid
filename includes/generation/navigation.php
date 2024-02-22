367bf4e466ae8989ed74715adfc4d1326abbdef4
RundesBalli <GitHub@RundesBalli.com>
2024-02-22 21:04:08 +0100

    Added itemCount to navigation elements

--- includes/generation/navigation.php
+++ includes/generation/navigation.php
@@ -9,14 +9,32 @@ $nav = '<a id="toggleElement"></a>';
 $nav.= '<a href="/" class="title">pr0gramm-Spendenraid</a>';
 
 if(empty($loggedIn)) {
+  /**
+   * Navigation elements when not logged in.
+   */
   $nav.= '<a href="/"'.($route == 'login' ? $a : NULL).'>'.$lang['nav']['login'].'</a>';
   $nav.= '<a href="https://RundesBalli.com" target="_blank" rel="noopener">RundesBalli</a>';
   $nav.= '<a href="https://pr0gramm.com/inbox/messages/RundesBalli" target="_blank" rel="noopener">'.$lang['nav']['contact'].'</a>';
   $nav.= '<a href="https://github.com/RundesBalli/pr0gramm-Spendenraid" target="_blank" rel="noopener">'.$lang['nav']['github'].'</a>';
 } else {
+  /**
+   * Display of the item count which can be evaluated by the user.
+   */
+  // Evaluation
+  $result = mysqli_query($dbl, "SELECT count(`id`) AS `c` FROM `items` WHERE `firstsightValue` IS NULL OR (`confirmedValue` IS NULL AND `firstsightUserId` != '".$userId."')") OR DIE(MYSQLI_ERROR($dbl));$qc++;
+  $row = mysqli_fetch_assoc($result);
+  $valCount = $row['c'];
+  // Organizations
+  $result = mysqli_query($dbl, "SELECT count(`id`) AS `c` FROM `items` WHERE `isDonation`='1' AND (`firstsightOrgaId` IS NULL OR (`confirmedOrgaId` IS NULL AND `firstsightOrgaUserId`!='".$userId."'))") OR DIE(MYSQLI_ERROR($dbl));$qc++;
+  $row = mysqli_fetch_assoc($result);
+  $orgaCount = $row['c'];
+
+  /**
+   * Navigation Elements when logged in.
+   */
   $nav.= '<a href="/overview"'.($route == 'overview' ? $a : NULL).'>'.$lang['nav']['overview'].'</a>';
-  $nav.= '<a href="/evaluation"'.($route == 'evaluation' ? $a : NULL).'>'.$lang['nav']['evaluation'].'</a>';
-  $nav.= '<a href="/orga"'.($route == 'orga' ? $a : NULL).'>'.$lang['nav']['orga'].'</a>';
+  $nav.= '<a href="/evaluation"'.($route == 'evaluation' ? $a : NULL).'>'.$lang['nav']['evaluation'].(!empty($valCount) ? " (".$valCount.")" : NULL).'</a>';
+  $nav.= '<a href="/orga"'.($route == 'orga' ? $a : NULL).'>'.$lang['nav']['orga'].(!empty($orgaCount) ? " (".$orgaCount.")" : NULL).'</a>';
   $nav.= '<a href="/postInfo"'.($route == 'postInfo' ? $a : NULL).'>'.$lang['nav']['postInfo'].'</a>';
   $nav.= '<a href="/log"'.($route == 'log' ? $a : NULL).'>'.$lang['nav']['log'].'</a>';
   $nav.= '<a href="/stats"'.($route == 'stats' ? $a : NULL).'>'.$lang['nav']['stats'].'</a>';
