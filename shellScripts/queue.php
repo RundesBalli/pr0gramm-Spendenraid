92ff2f859c7562a3b7498144335ee3ade25d6adc
RundesBalli <GitHub@RundesBalli.com>
2024-03-27 08:08:41 +0100

    Try to fix unknown bug :D

--- shellScripts/queue.php
+++ shellScripts/queue.php
@@ -51,8 +51,18 @@ while($row = mysqli_fetch_assoc($result)) {
   /**
    * Get sums and counts.
    */
-  $innerResult = mysqli_query($dbl, 'SELECT (SELECT IFNULL(sum(`confirmedValue`), 0) FROM `items` WHERE `username`="'.defuse($row['name']).'" AND `confirmedOrgaId`!=9 AND `isDonation`=1) as `confirmedValue`, (SELECT count(`id`) FROM `items` WHERE `username`="'.defuse($row['name']).'" AND `confirmedOrgaId`=9 AND `isDonation`=2) as `goodActs`') OR DIE(MYSQLI_ERROR($dbl));
-  $innerRow = mysqli_fetch_assoc($innerResult);
+  #$innerResult = mysqli_query($dbl, 'SELECT (SELECT IFNULL(sum(`confirmedValue`), 0) FROM `items` WHERE `username`="'.defuse($row['name']).'" AND `confirmedOrgaId`!=9 AND `isDonation`=1) as `confirmedValue`, (SELECT count(`id`) FROM `items` WHERE `username`="'.defuse($row['name']).'" AND `confirmedOrgaId`=9 AND `isDonation`=2) as `goodActs`') OR DIE(MYSQLI_ERROR($dbl));
+  #$innerRow = mysqli_fetch_assoc($innerResult);
+  $innerResult = mysqli_query($dbl, 'SELECT * FROM `items` WHERE `username`="'.defuse($row['name']).'"') OR DIE(MYSQLI_ERROR($dbl));
+  $confirmedValue = 0;
+  $goodActs = 0;
+  while($innerRow = mysqli_fetch_assoc($innerResult)) {
+    if($innerRow['isDonation'] == 1) {
+      $confirmedValue += $innerRow['confirmedValue'];
+    } elseif($innerRow['isDonation'] == 2) {
+      $goodActs++;
+    }
+  }
 
   /**
    * Check whether the user should be unlocked or locked.
@@ -61,11 +71,11 @@ while($row = mysqli_fetch_assoc($result)) {
     /**
      * User should be unlocked, so it is necessary to check if the unlocking is really allowed.
      */
-    if($innerRow['confirmedValue'] > 0 OR $innerRow['goodActs'] > 0) {
+    if($confirmedValue > 0 OR $goodAct > 0) {
       /**
        * Unlock the user.
        */
-      $response = apiCall('https://pr0gramm.com/api/casino/unlockUser', ['secret' => $perkSecret, 'name' => $row['name'], 'confirmedValue' => floatval($innerRow['confirmedValue']), 'goodActs' => intval($innerRow['goodActs'])]);
+      $response = apiCall('https://pr0gramm.com/api/casino/unlockUser', ['secret' => $perkSecret, 'name' => $row['name'], 'confirmedValue' => floatval($confirmedValue), 'goodActs' => intval($goodActs)]);
       if($response['success'] == TRUE) {
         /**
          * All right. Remove the entry from the queue.
@@ -86,14 +96,14 @@ while($row = mysqli_fetch_assoc($result)) {
       mysqli_query($dbl, 'UPDATE `queue` SET `error`=1 WHERE `id`='.$row['id'].' LIMIT 1') OR DIE(MYSQLI_ERROR($dbl));
       #mysqli_query($dbl, 'INSERT INTO `queue` (`name`, `action`) VALUE ("'.defuse($row['name']).'", 0)') OR DIE(MYSQLI_ERROR($dbl));
       #mysqli_query($dbl, 'INSERT INTO `log` (`logLevel`, `text`) VALUES (6, "'.sprintf($lang['cli']['queue']['justInCaseLock'], defuse($row['name']), $innerRow['confirmedValue'], $innerRow['goodActs']).'")') OR DIE(MYSQLI_ERROR($dbl));
-      mysqli_query($dbl, 'INSERT INTO `log` (`logLevel`, `text`) VALUES (6, "'.sprintf($lang['cli']['queue']['unknownError'], defuse($row['name']), $innerRow['confirmedValue'], $innerRow['goodActs']).'")') OR DIE(MYSQLI_ERROR($dbl));
+      mysqli_query($dbl, 'INSERT INTO `log` (`logLevel`, `text`) VALUES (6, "'.sprintf($lang['cli']['queue']['unknownError'], defuse($row['name']), $confirmedValue, $goodActs).'")') OR DIE(MYSQLI_ERROR($dbl));
     }
   } else {
     /**
      * User should be locked. If the user has any donations or good acts, the user will not be locked and the
      * queue will silently continue.
      */
-    if($innerRow['confirmedValue'] == 0 AND $innerRow['goodActs'] == 0) {
+    if($confirmedValue == 0 AND $goodActs == 0) {
       /**
        * The user has no donations or good acts, so the locking will take place.
        */
@@ -118,5 +128,6 @@ while($row = mysqli_fetch_assoc($result)) {
       mysqli_query($dbl, 'DELETE FROM `queue` WHERE `name`="'.$row['name'].'" AND `action`=0') OR DIE(MYSQLI_ERROR($dbl));
     }
   }
+  usleep(300000);
 }
 ?>
