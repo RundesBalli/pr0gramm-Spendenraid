b34f2f0a7a056d1c0d6e6894f1ef6c8186cdd072
RundesBalli <GitHub@RundesBalli.com>
2025-03-17 20:31:28 +0100

    Removed goodActs, updated to newer sql version

--- sql/spendenraid.sql
+++ sql/spendenraid.sql
@@ -1,4 +1,4 @@
--- Adminer 4.8.1 MySQL 5.5.5-10.1.48-MariaDB-0+deb9u2 dump
+-- Adminer 4.8.1 MySQL 10.11.6-MariaDB-0+deb12u1 dump
 
 SET NAMES utf8;
 SET time_zone = '+00:00';
@@ -7,17 +7,17 @@ SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 SET NAMES utf8mb4;
 
-DROP DATABASE IF EXISTS `spendenraid`;
-CREATE DATABASE `spendenraid` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
-USE `spendenraid`;
+DROP DATABASE IF EXISTS `spendenraid_2025`;
+CREATE DATABASE `spendenraid_2025` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `spendenraid_2025`;
 
 DROP TABLE IF EXISTS `fakes`;
 CREATE TABLE `fakes` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `itemIdOriginal` int(10) unsigned NOT NULL COMMENT 'items.itemId',
   `itemIdFake` int(10) unsigned NOT NULL COMMENT 'items.itemId',
-  `certain` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0 = not sure; 1 = sure',
-  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Timestamp of the entry',
+  `certain` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT '0 = not sure; 1 = sure',
+  `timestamp` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Timestamp of the entry',
   PRIMARY KEY (`id`),
   UNIQUE KEY `itemIdOriginal_itemIdFake` (`itemIdOriginal`,`itemIdFake`),
   KEY `certain` (`certain`),
@@ -38,26 +38,26 @@ CREATE TABLE `items` (
   `down` int(10) unsigned NOT NULL COMMENT 'Downvotes',
   `benis` int(10) NOT NULL COMMENT 'Sum of up- and downvotes',
   `created` datetime NOT NULL COMMENT 'Timestamp of the post',
-  `image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Image URL',
-  `thumb` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Thumbnail URL',
-  `fullsize` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Fullsize image URL',
+  `image` varchar(255) NOT NULL COMMENT 'Image URL',
+  `thumb` varchar(255) NOT NULL COMMENT 'Thumbnail URL',
+  `fullsize` varchar(255) DEFAULT NULL COMMENT 'Fullsize image URL',
   `width` int(10) unsigned NOT NULL COMMENT 'Width of the image',
   `height` int(10) unsigned NOT NULL COMMENT 'Height of the image',
   `audio` tinyint(1) unsigned NOT NULL COMMENT '0 = no audio; 1 = audio',
-  `extension` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'e.g. png or jpeg',
+  `extension` varchar(5) NOT NULL COMMENT 'e.g. png or jpeg',
   `flags` tinyint(2) unsigned NOT NULL COMMENT 'N/SFW/L/P / Pol',
-  `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Username',
+  `username` varchar(50) NOT NULL COMMENT 'Username',
   `mark` tinyint(2) unsigned NOT NULL COMMENT 'Usermark',
   `firstsightValue` double(10,2) unsigned DEFAULT NULL COMMENT 'NULL or evaluated value',
   `firstsightUserId` int(10) unsigned DEFAULT NULL COMMENT 'NULL or users.id of the first sight user',
   `confirmedValue` double(10,2) unsigned DEFAULT NULL COMMENT 'NULL or evaluated value',
   `confirmedUserId` int(10) unsigned DEFAULT NULL COMMENT 'NULL or users.id of the confirming user',
-  `isDonation` tinyint(1) unsigned DEFAULT NULL COMMENT 'Initial NULL; 0 = no donation; 1 = donation; 2 = good act',
+  `isDonation` tinyint(1) unsigned DEFAULT NULL COMMENT 'Initial NULL; 0 = no donation; 1 = donation',
   `firstsightOrgaId` int(10) unsigned DEFAULT NULL COMMENT 'NULL or first sight orgas.id',
   `firstsightOrgaUserId` int(10) unsigned DEFAULT NULL COMMENT 'NULL or users.id of the first sight user',
   `confirmedOrgaId` int(10) unsigned DEFAULT NULL COMMENT 'NULL or confirmed orgas.id',
   `confirmedOrgaUserId` int(10) unsigned DEFAULT NULL COMMENT 'NULL or users.id of the confirming user',
-  `delFlag` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0 = item exists on pr0gramm; 1 = item has been deleted or can no longer be found using the search pattern',
+  `delFlag` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT '0 = item exists on pr0gramm; 1 = item has been deleted or can no longer be found using the search pattern',
   PRIMARY KEY (`id`),
   UNIQUE KEY `itemId` (`itemId`),
   KEY `promoted` (`promoted`),
@@ -85,10 +85,10 @@ DROP TABLE IF EXISTS `log`;
 CREATE TABLE `log` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `userId` int(10) unsigned DEFAULT NULL COMMENT 'users.id',
-  `timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Timestamp of the log entry',
+  `timestamp` datetime NOT NULL DEFAULT current_timestamp() COMMENT 'Timestamp of the log entry',
   `logLevel` int(10) unsigned NOT NULL COMMENT 'metaLogLevel.id',
   `itemId` int(10) unsigned DEFAULT NULL COMMENT 'items.itemId',
-  `text` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Log text',
+  `text` varchar(255) DEFAULT NULL COMMENT 'Log text',
   PRIMARY KEY (`id`),
   KEY `timestamp` (`timestamp`),
   KEY `logLevel` (`logLevel`),
@@ -103,8 +103,8 @@ CREATE TABLE `log` (
 DROP TABLE IF EXISTS `metaLogLevel`;
 CREATE TABLE `metaLogLevel` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `type` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Log entry type',
-  `color` varchar(6) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Color hex for the log entry',
+  `type` varchar(50) NOT NULL COMMENT 'Log entry type',
+  `color` varchar(6) NOT NULL COMMENT 'Color hex for the log entry',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Log level types';
 
@@ -122,37 +122,35 @@ INSERT INTO `metaLogLevel` (`id`, `type`, `color`) VALUES
 DROP TABLE IF EXISTS `metaOrganizations`;
 CREATE TABLE `metaOrganizations` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `name` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Name of the organization',
-  `shortName` varchar(20) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Short name of the organization (for fast evaluation navigation)',
-  `sortIndex` int(10) unsigned NOT NULL DEFAULT '9999999' COMMENT 'Sort index; 0 = higher',
-  `exportCountOnly` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '0 = normal export; 1 = only export the count',
-  `exportSortIndex` int(10) unsigned NOT NULL DEFAULT '9999999' COMMENT 'Sort index for the export; 0 = higher',
+  `name` varchar(200) NOT NULL COMMENT 'Name of the organization',
+  `shortName` varchar(20) DEFAULT NULL COMMENT 'Short name of the organization (for fast evaluation navigation)',
+  `sortIndex` int(10) unsigned NOT NULL DEFAULT 9999999 COMMENT 'Sort index; 0 = higher',
+  `exportSortIndex` int(10) unsigned NOT NULL DEFAULT 9999999 COMMENT 'Sort index for the export; 0 = higher',
   PRIMARY KEY (`id`),
   KEY `sortIndex` (`sortIndex`),
   KEY `exportSortIndex` (`exportSortIndex`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Organizations';
 
 TRUNCATE `metaOrganizations`;
-INSERT INTO `metaOrganizations` (`id`, `name`, `shortName`, `sortIndex`, `exportCountOnly`, `exportSortIndex`) VALUES
-(1,	'Deutsche Knochenmarkspenderdatei (DKMS)',	'DKMS',	10,	0,	20),
-(2,	'Deutsche Krebshilfe (auch dt. Kinderkrebshilfe)',	'DtKrebshilfe',	20,	0,	10),
-(3,	'Deutsches Krebsforschungszentrum (DKFZ)',	NULL,	30,	0,	30),
-(4,	'Deutsche Kinderkrebsstiftung',	NULL,	40,	0,	40),
-(5,	'Österreichische Spendenorganisationen',	NULL,	50,	0,	50),
-(6,	'Schweizer Spendenorganisationen',	NULL,	60,	0,	60),
-(7,	'diverse andere',	NULL,	110,	0,	110),
-(8,	'nicht ersichtlich',	NULL,	120,	0,	120),
-(9,	'Gute Tat',	NULL,	130,	1,	130),
-(10,	'Sonstige Depressionshilfe',	NULL,	70,	0,	70),
-(11,	'Sonstige Tier-/Naturschutzorganisationen',	NULL,	80,	0,	80),
-(12,	'Ukraine Nothilfe',	NULL,	90,	0,	90),
-(13,	'DRK ohne Ukraine',	NULL,	100,	0,	100);
+INSERT INTO `metaOrganizations` (`id`, `name`, `shortName`, `sortIndex`, `exportSortIndex`) VALUES
+(1,	'Deutsche Knochenmarkspenderdatei (DKMS)',	'DKMS',	10,	20),
+(2,	'Deutsche Krebshilfe (auch dt. Kinderkrebshilfe)',	'DtKrebshilfe',	20,	10),
+(3,	'Deutsches Krebsforschungszentrum (DKFZ)',	NULL,	30,	30),
+(4,	'Deutsche Kinderkrebsstiftung',	NULL,	40,	40),
+(5,	'Österreichische Spendenorganisationen',	NULL,	50,	50),
+(6,	'Schweizer Spendenorganisationen',	NULL,	60,	60),
+(7,	'diverse andere',	NULL,	110,	110),
+(8,	'nicht ersichtlich',	NULL,	120,	120),
+(10,	'Sonstige Depressionshilfe',	NULL,	70,	70),
+(11,	'Sonstige Tier-/Naturschutzorganisationen',	NULL,	80,	80),
+(12,	'Ukraine Nothilfe',	NULL,	90,	90),
+(13,	'DRK ohne Ukraine',	NULL,	100,	100);
 
 DROP TABLE IF EXISTS `permissions`;
 CREATE TABLE `permissions` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Name of the permission',
-  `description` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Description',
+  `name` varchar(50) NOT NULL COMMENT 'Name of the permission',
+  `description` varchar(250) NOT NULL COMMENT 'Description',
   PRIMARY KEY (`id`),
   KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Special permissions';
@@ -167,9 +165,9 @@ INSERT INTO `permissions` (`id`, `name`, `description`) VALUES
 DROP TABLE IF EXISTS `queue`;
 CREATE TABLE `queue` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Username',
+  `name` varchar(50) NOT NULL COMMENT 'Username',
   `action` tinyint(1) unsigned NOT NULL COMMENT '1 = unlock; 0 = lock',
-  `error` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '1 = error',
+  `error` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT '1 = error',
   PRIMARY KEY (`id`),
   KEY `error` (`error`),
   KEY `name` (`name`)
@@ -180,8 +178,8 @@ DROP TABLE IF EXISTS `sessions`;
 CREATE TABLE `sessions` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `userId` int(10) unsigned NOT NULL COMMENT 'users.id',
-  `hash` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Hash',
-  `lastActivity` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP COMMENT 'Time of last activity',
+  `hash` varchar(64) NOT NULL COMMENT 'Hash',
+  `lastActivity` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp() COMMENT 'Time of last activity',
   PRIMARY KEY (`id`),
   KEY `userId` (`userId`),
   KEY `hash` (`hash`),
@@ -205,14 +203,14 @@ CREATE TABLE `userPermissions` (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Username',
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Hashed password',
-  `salt` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'Salt',
-  `bot` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '1 = bot account',
+  `name` varchar(50) NOT NULL COMMENT 'Username',
+  `password` varchar(255) DEFAULT NULL COMMENT 'Hashed password',
+  `salt` varchar(255) DEFAULT NULL COMMENT 'Salt',
+  `bot` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT '1 = bot account',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`),
   KEY `bot` (`bot`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
--- 2024-02-25 21:43:52
+-- 2025-03-17 19:26:40
